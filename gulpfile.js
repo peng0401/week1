a1c268f9f959d4c2e3907f30ec0308cce02cfa4a
peng0401 <814162169@qq.com>
2018-12-03 09:54:22 +0800

    watch完成

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,21 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass")
 var minCss = require("gulp-clean-css")
+var server = require("gulp-webserver")
+var uglify = require("gulp-uglify");
+var path = require("path")
+var url = require("url")
+var fs = require("fs")
 gulp.task("sass",() => {
     return gulp.src('./src/scss/*.scss')
     .pipe(sass())
     .pipe(minCss())
-    .pipe(gulp.dest('./src/css/')) 
+    .pipe(gulp.dest('./src/css')) 
 })
+
+gulp.task("watch",() => {
+    return gulp.watch('./src/scss/*.scss',gulp.series('sass'))
+})
+
+gulp.task('dev',gulp.series('sass','watch'))
 
